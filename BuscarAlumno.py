2e02b6da4edf0085e1820b301f4580f25131d211
Paul Ricardo Maguiña Quispe <paul.maguina@utec.edu.pe>
2026-06-13 21:37:48 -0500

    Add files via upload

---
+++ BuscarAlumno.py
@@ -0,0 +1,30 @@
+import boto3
+
+def lambda_handler(event, context):
+    # Entrada (json)
+    tenant_id = event['body']['tenant_id']
+    alumno_id = event['body']['alumno_id']
+    
+    # Proceso
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('t_alumnos')
+    
+    response = table.get_item(
+        Key={
+            'tenant_id': tenant_id,
+            'alumno_id': alumno_id
+        }
+    )
+    
+    # Salida (json)
+    # Validamos si el registro existe
+    if 'Item' in response:
+        return {
+            'statusCode': 200,
+            'alumno': response['Item']
+        }
+    else:
+        return {
+            'statusCode': 404,
+            'mensaje': 'Alumno no encontrado'
+        }
